78e676bb279e7dac032c3457d932f99be777b05d
pasanprem <46164432+pasanprem@users.noreply.github.com>
2020-07-12 17:30:41 +0100

    Add project files.

---
+++ ChessBoardModel/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    class Cell
+    {
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+        public bool CurrentlyOccupied { get; set; }
+        public bool LegalNextMove { get; set; }
+    }
+}
